5ec798749a282b9085df33b0f1e415d7287865f6
Richie Wong <richiewong07@yahoo.com>
2018-02-09 18:00:16 -0600

    updated

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 // RUN OUR JQUERY
 $(document).ready(function() {
+  var following = [];
   // FREE CODE CAMP STREAM INFO AND STATUS API CALL
   var url = "https://wind-bow.glitch.me/twitch-api?callback=?";
   $.getJSON(url, function(data1) {
@@ -9,4 +10,16 @@ $(document).ready(function() {
       $("#fccStatus").html("Free Code Camp is Currently Online!");
     }
   });
+
+  var followerURL = "https://api.twitch.tv/kraken/users/freecodecamp/follows/channels/";
+  $.getJSON(followerURL, function(data2) {
+    for (var i = 0; i < data2.follows.length; i++) {
+      var displayName = data2.follows[i].channel.display_name;
+      following.push(displayName);
+    }
+    following.push('comster404');
+    following.push('brunofin');
+    following.push('ESL_SC2');
+  });
+
 })
